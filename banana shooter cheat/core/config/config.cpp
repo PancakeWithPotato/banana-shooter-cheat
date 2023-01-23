9b5a5fe1b0f23ab4c8aff545374e8f0964940bec
Pancake <90145984+PancakeWithPotato@users.noreply.github.com>
2023-01-23 20:57:45 +0100

    config fixing

--- banana shooter cheat/core/config/config.cpp
+++ banana shooter cheat/core/config/config.cpp
@@ -3,79 +3,101 @@
 
 #include "INIREADER.h"
 
+void g_Config::Base::Update(const int& mode)
+{
+	switch (mode) {
+	case 1:
+		elements["b:combat:aimbot_enabled"] = g_Config::Combat::Aimbot;
+		elements["i:combat:aimbot_target"] = g_Config::Combat::AimbotHitbox;
+		elements["b:combat:noreload"] = g_Config::Combat::NoReload;
+		elements["b:combat:norecoil"] = g_Config::Combat::NoRecoil;
+		break;
+	case 2:
+		g_Config::Combat::Aimbot = std::get<bool>(elements["b:combat:aimbot_enabled"]);
+		g_Config::Combat::AimbotHitbox = std::get<int>(elements["i:combat:aimbot_target"]);
+		g_Config::Combat::NoReload = std::get<bool>(elements["b:combat:noreload"]);
+		g_Config::Combat::NoRecoil = std::get<bool>(elements["b:combat:norecoil"]);
+		break;
+	}
+	
+}
+
 void g_Config::Base::Init()
 {
 	std::string strLocaluser = (g_Hack->strName == "dev" ? "Pancake" : g_Hack->strName); //works tho
-	std::string strLocalpath = "C:\\Users\\" + strLocaluser + "\\Documents\\meowware";
-	if(!_mkdir(strLocalpath.c_str()))
+    strBase = "C:\\Users\\" + strLocaluser + "\\Documents\\meowware";
+	if(!_mkdir(strBase.c_str()))
 		g_Debug.logState(::SUCCESS, "Created directory 1!");
 
-	strLocalpath.append("\\banana_shooter");
-	if (!_mkdir(strLocalpath.c_str()))
+	strBase.append("\\banana_shooter");
+	if (!_mkdir(strBase.c_str()))
 		g_Debug.logState(::SUCCESS, "Created directory 2!");
 
 	g_Config::Base::strConfigs.reserve(1); //surely at least 1 config exists/will be made
 
 	//set up elements
-	elements["b:combat:aimbot_enabled"] = false;
-	elements["i:combat:aimbot_target"] = 0;
+	elements["b:combat:aimbot_enabled"] = g_Config::Combat::Aimbot;
+	elements["i:combat:aimbot_target"] = g_Config::Combat::AimbotHitbox;
 	elements["b:combat:noreload"] = false;
 	elements["b:combat:norecoil"] = false;
 }
 
-//thx stephan (heavily inspired)
-void g_Config::Base::Load(const std::string& strName) 
+void g_Config::Base::Save(const std::string& strName) 
 {
-	if (!std::filesystem::exists(strName)) {
-		g_Debug.logState(::ERROR, "Config " + strName + " doesn't exist!");
+	std::ofstream file;
+	file.open(g_Config::Base::strBase + "\\" + strName);
+	if (!file.is_open())
 		return;
-	}
 
-	INIReader configParser(strName);
+	Update(1);
+	g_Debug.logState(::SUCCESS, "Succesfully opened config " + strName);
 
-	if (configParser.ParseError()) {
-		g_Debug.logState(::ERROR, "Couldn't load file " + strName);
-		return;
-	}
-
+	//thx @stephan
 	static auto split = [](const std::string& s, char delim) {
 		std::vector<std::string> result;
 		std::stringstream ss(s);
 		std::string item;
-
+	
 		while (getline(ss, item, delim))
 			result.push_back(item);
-
+	
 		return result;
 	};
+	std::string strCurrentSection = "";
 
-	for (auto& i : elements) {
-		std::vector<std::string> splitted = split(i.first, ':');
-
-		switch (splitted[0][0]) {
-		case 'i': elements[i.first] = configParser.GetInteger(splitted[1], splitted[2], 0); break;
-		case 'f': elements[i.first] = configParser.GetReal(splitted[1], splitted[2], 0.0f); break;
-		case 'b': elements[i.first] = configParser.GetBoolean(splitted[1], splitted[2], false); break;
-		case 'c': {
-			std::string strCol = configParser.Get(splitted[1], splitted[2], "255,255,255,255");
-			std::vector<std::string> splittedCol = split(strCol, ',');
-
-			if (splittedCol.size() != 4)
-				continue;
-
-			elements[i.first] = ImColor(std::stoi(splittedCol[0]), std::stoi(splittedCol[1]), std::stoi(splittedCol[2]), std::stoi(splittedCol[3]));
+	for (auto& i : g_Config::Base::elements) 
+	{
+		std::vector<std::string> strSplitted = split(i.first, ':');
+		if (strCurrentSection == "" || strCurrentSection != strSplitted[1]) 
+		{
+			strCurrentSection = strSplitted[1];
+			file << "[" << strCurrentSection << "]\n";
+		}
+		switch (strSplitted[0][0])
+		{
+		case 'i':
+			file << strSplitted[2] << " = " << std::to_string(std::get<int>(i.second)) << "\n";
+			break;
+		case 'f':
+			file << strSplitted[2] << " = " << std::to_string(std::get<float>(i.second)) << "\n";
+			break;
+		case 'b':
+			file << strSplitted[2] << " = " << std::to_string(std::get<bool>(i.second)) << "\n";
+			break;
+		default:
+			g_Debug.logState(::WARNING, "Its a color");
 			break;
 		}
-		}
+
 	}
 
-	g_Debug.logState(::SUCCESS, "Loaded config!");
+	file.close();
+	g_Debug.logState(::SUCCESS, "Loaded in config " + strName);
 }
 
-//again, thx stephan (heavily inspired)
-void g_Config::Base::Save(const std::string& strName) {
-	g_Debug.logState(::WARNING, "Saving config " + strName);
-
+void g_Config::Base::Load(const std::string& strName) 
+{
+	//thx @stephan again
 	static auto split = [](const std::string& s, char delim) {
 		std::vector<std::string> result;
 		std::stringstream ss(s);
@@ -87,35 +109,32 @@ void g_Config::Base::Save(const std::string& strName) {
 		return result;
 	};
 
-	std::string previousSection = "";
+	std::string strParser = g_Config::Base::strBase + "\\" +  strName;
+	INIReader parser(strParser);
 
-	std::ofstream write(strName);
+	if (parser.ParseError())
+		return;
 
-	for (auto i : elements) {
-		std::vector<std::string> splitted = split(i.first, ':');
+	g_Debug.logState(::SUCCESS, "Opened up config " + strName + " for loading");
 
-		if (previousSection != splitted[1]) {
-			write << "\n[" << splitted[1] << "]\n";
-			previousSection = splitted[1];
-		}
-
-		switch (splitted[0][0]) {
-		case 'i': write << splitted[2] << " = " << std::to_string(std::get<int>(i.second)); break;
-		case 'f': write << splitted[2] << " = " << std::to_string(std::get<float>(i.second)); break;
-		case 'b': write << splitted[2] << " = " << std::to_string(std::get<bool>(i.second)); break;
-		case 'c': {
-			ImColor col = std::get<ImColor>(i.second);
-
-			write << splitted[2] << " = " << std::to_string(col.Value.x) << ", " << std::to_string(col.Value.y) << ", " << std::to_string(col.Value.z) << ", " << std::to_string(col.Value.w);
+	for (auto& i : elements) 
+	{
+		std::vector<std::string> strSplitted = split(i.first, ':');
+		switch (strSplitted[0][0])
+		{
+		case 'i':
+			elements[i.first] = parser.GetInteger(strSplitted[1], strSplitted[2], 0); 
+			break;
+		case 'f':
+			elements[i.first] = parser.GetReal(strSplitted[1], strSplitted[2], 0.f);
+			break;
+		case 'b':
+			elements[i.first] = parser.GetBoolean(strSplitted[1], strSplitted[2], true);
+			break;
+		default:
 			break;
 		}
-		}
-
-		write << '\n';
 	}
-
-	write.close();
-
-
-	g_Debug.logState(::SUCCESS, "Saved config!");
+	Update(2);
+	g_Debug.logState(::SUCCESS, "Loaded in config " + strName);
 }
--- banana shooter cheat/core/config/config.hpp
+++ banana shooter cheat/core/config/config.hpp
@@ -6,21 +6,16 @@
 #include <fstream>
 namespace g_Config 
 {
-	namespace Base {
+	namespace Base 
+	{
+		void Update(const int& mode);
 		void Init();
 		void Save(const std::string& strName);
 		void Load(const std::string& strName);
 		inline std::vector<std::string> strConfigs;
 		inline std::map<std::string, std::variant<int, bool, float, ImColor>> elements;
-		template <typename T>
-		inline T& get(std::string name) {
-			if (g_Config::Base::elements.count(name))
-				return std::get<T>(g_Config::Base::elements[name]);
-			else {
-				g_Debug.logState(::ERROR, "[Config] ERROR: Could not find config element: %s", name.c_str());
-				std::this_thread::sleep_for(std::chrono::seconds(5));
-			}
-		}
+		inline std::string strBase;
+
 	}
 	namespace Combat {
 		inline bool NoReload = false;
